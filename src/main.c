abc1b033c15a186ef92e9d195eb302fd9150ab9d
adamws <adamws@users.noreply.github.com>
2024-10-02 20:18:26 +0200

    Render some keys and display keyboard presses

--- src/main.c
+++ src/main.c
@@ -6,10 +6,62 @@
 #include <SDL_keycode.h>
 #include <SDL_rect.h>
 #include <SDL_render.h>
+#include <SDL_scancode.h>
 #include <SDL_timer.h>
 
 #include <math.h>
 #include <stdbool.h>
+
+#define ARRAY_LENGTH(x) (sizeof(x) / sizeof((x)[0]))
+
+struct Key {
+  const float x;
+  const float y;
+  const float rot;
+  bool pressed;
+} Key;
+
+struct Key keyboard[] = {
+  {0, 0, 0},
+  {1, 0, 0},
+  {0, 1, 0},
+  {1, 1, 0},
+};
+
+const int scancode_keyboard_lookup[256] = {
+  -1, -1, -1, -1,  2, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1,  0, -1,  3, -1,
+  -1, -1,  1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1,
+  -1, -1, -1, -1, -1, -1, -1, -1,
+};
 
 SDL_Texture* keycap_texture = NULL;
 int keycap_width = -1;
@@ -26,8 +78,21 @@ void update(float dT) {
 }
 
 void render(SDL_Renderer* renderer) {
-  SDL_Rect dst = { 0, 0, keycap_width, keycap_height };
-  SDL_RenderCopy(renderer, keycap_texture, NULL, &dst);
+  struct Key* k = NULL;
+
+  SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 128);
+
+  for (int i = 0; i < ARRAY_LENGTH(keyboard); i++) {
+    k = &keyboard[i];
+    int x = keycap_width * k->x;
+    int y = keycap_height * k->y;
+    SDL_Rect dst = { x, y, keycap_width, keycap_height };
+    SDL_RenderCopy(renderer, keycap_texture, NULL, &dst);
+    if (k->pressed) {
+        SDL_RenderFillRect(renderer, &dst);
+    }
+  }
 }
 
 void loop(SDL_Renderer* renderer) {
@@ -39,6 +104,8 @@ void loop(SDL_Renderer* renderer) {
     Uint64 start = SDL_GetPerformanceCounter();
 
     SDL_Event event;
+    int scancode = -1;
+    int index = -1;
 
     SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
     SDL_RenderClear(renderer);
@@ -50,17 +117,27 @@ void loop(SDL_Renderer* renderer) {
           running = false;
           break;
         case SDL_KEYDOWN:
-          switch (event.key.keysym.sym) {
-              case SDLK_ESCAPE:
+          scancode = event.key.keysym.scancode;
+          switch (scancode) {
+              case SDL_SCANCODE_ESCAPE:
                   running = false;
                   break;
               default:
-                  printf("Pressed: %d\n", event.key.keysym.scancode);
+                  index = scancode_keyboard_lookup[scancode];
+                  printf("Down: %d; index: %d\n", scancode, index);
+                  if (index >= 0) {
+                    keyboard[index].pressed = true;
+                  }
                   break;
           }
           break;
         case SDL_KEYUP:
-          printf("Pressed: %d\n", event.key.keysym.scancode);
+          scancode = event.key.keysym.scancode;
+          index = scancode_keyboard_lookup[scancode];
+          printf("Up: %d; index: %d\n", scancode, index);
+          if (index >= 0) {
+            keyboard[index].pressed = false;
+          }
           break;
         default:
           break;
@@ -96,8 +173,8 @@ int main(int argc, char** argv) {
   SDL_Window* window = NULL;
   SDL_Renderer* renderer = NULL;
 
-  int width = 1960;
-  int height = 1280;
+  int width = 960;
+  int height = 320;
   SDL_CreateWindowAndRenderer(width, height, 0, &window, &renderer);
 
   SDL_Surface* keycap_surface = IMG_Load("assets/keycap.png");
