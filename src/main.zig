570a210ad212bba81ecfc98627196c399899abda
adamws <adamws@users.noreply.github.com>
2024-10-26 16:14:56 +0200

    Introduce X11InputContext struct, reduced code duplication

--- src/main.zig
+++ src/main.zig
@@ -18,16 +18,16 @@ const glfw = struct {
     pub extern fn glfwGetX11Window(window: ?*GLFWwindow) x11.Window;
 };
 
-pub const KEY_1U_PX = 64;
+const KEY_1U_PX = 64;
 
-pub const KeyOnScreen = struct {
+const KeyOnScreen = struct {
     src: rl.Rectangle,
     dst: rl.Rectangle,
     angle: f32,
     pressed: bool,
 };
 
-pub const KeyData = struct {
+const KeyData = struct {
     pressed: bool,
     repeated: bool,
     keycode: x11.KeyCode,
@@ -76,6 +76,84 @@ const CodepointBuffer = struct {
     }
 };
 
+const X11InputContext = struct {
+    display: *x11.Display,
+    xim: x11.XIM,
+    xic: x11.XIC,
+
+    pub fn init(display: *x11.Display, window: x11.Window) !X11InputContext {
+        const xim = x11.XOpenIM(display, null, null, null) orelse {
+            std.debug.print("Cannot initialize input method\n", .{});
+            return error.X11InitializationFailed;
+        };
+
+        const xic = createIC(xim, .{
+            x11.XNInputStyle,
+            x11.XIMPreeditNothing | x11.XIMStatusNothing,
+            x11.XNClientWindow,
+            window,
+            x11.XNFocusWindow,
+            window,
+        });
+        if (xic == null) {
+            std.debug.print("Cannot initialize input context\n", .{});
+            return error.X11InitializationFailed;
+        }
+
+        return .{
+            .display = display,
+            .xim = xim,
+            .xic = xic,
+        };
+    }
+
+    // This solves problem with calling x11.XCreateIC directly which cause segfault on Debug build
+    // (but works on ReleaseSafe build), don't ask me why. This problem started to occur when I
+    // moved these functions to this struct.
+    // Previously, when called directly in x11Listener, it worked.
+    // Taken from:
+    // https://ziggit.dev/t/calling-variadic-c-functions/2894
+    // NOTE: checked with zig 0.14 release. Maybe this become unnecessary later.
+    // NOTE2: this might not solve anything after all, after some recompiles crashes anyway
+    fn createIC(xim: x11.XIM, args: anytype) x11.XIC {
+        return @call(.auto, x11.XCreateIC, .{xim} ++ args);
+    }
+
+    pub fn deinit(self: *X11InputContext) void {
+        x11.XDestroyIC(self.xic);
+        _ = x11.XCloseIM(self.xim);
+    }
+
+    pub fn lookupString(
+        self: *X11InputContext,
+        device_event: *const x11.XIDeviceEvent,
+        key_data: *KeyData,
+    ) c_int {
+        var e: x11.XKeyPressedEvent = std.mem.zeroInit(
+            x11.XKeyPressedEvent,
+            .{
+                .type = x11.XI_KeyPress,
+                .display = self.display,
+                .time = device_event.time,
+                .state = @as(c_uint, @intCast(device_event.mods.effective)),
+                .keycode = @as(c_uint, @intCast(device_event.detail)),
+                .same_screen = 1,
+            },
+        );
+        var status: x11.Status = undefined;
+        const len = x11.Xutf8LookupString(
+            self.xic,
+            &e,
+            &key_data.string,
+            32,
+            &key_data.keysym,
+            &status,
+        );
+        key_data.symbol = x11.XKeysymToString(key_data.keysym);
+        return len;
+    }
+};
+
 var keyboard: kle.Keyboard = undefined;
 var key_states: []KeyOnScreen = undefined;
 var keycode_keyboard_lookup = [_]i32{-1} ** 256;
@@ -93,7 +171,7 @@ const all_text = text ++ symbols;
 const font_data = @embedFile("resources/Hack-Regular.ttf");
 
 const record_events = false;
-const replay_events = true;
+const replay_events = false;
 
 fn xiSetMask(ptr: []u8, event: usize) void {
     const offset: u3 = @truncate(event);
@@ -120,13 +198,17 @@ fn x11Listener(app_window: x11.Window) !void {
     defer {
         std.debug.print("defer x11Listener\n", .{});
     }
-    var display: ?*x11.Display = null;
+
+    const display: *x11.Display = x11.XOpenDisplay(null) orelse {
+        std.debug.print("Unable to connect to X server\n", .{});
+        return error.X11InitializationFailed;
+    };
 
     var event: c_int = 0;
     var err: c_int = 0;
     var xi_opcode: i32 = 0;
 
-    // TODO: use buffered writer, to do that we must gracefuly handle this thread exit,
+    // TODO: use buffered writer, to do that we must gracefully handle this thread exit,
     // otherwise there is no good place to ensure writer flush
     var event_file: ?std.fs.File = null;
     if (record_events) {
@@ -135,45 +217,20 @@ fn x11Listener(app_window: x11.Window) !void {
     }
     defer event_file.?.close();
 
-    display = x11.XOpenDisplay(null);
-    if (display == null) {
-        std.debug.print("Unable to connect to X server\n", .{});
-        return error.X11InitializationFailed;
-    }
-
-    if (x11.XQueryExtension(display.?, "XInputExtension", &xi_opcode, &event, &err) == 0) {
+    if (x11.XQueryExtension(display, "XInputExtension", &xi_opcode, &event, &err) == 0) {
         std.debug.print("X Input extension not available.\n", .{});
         return error.X11InitializationFailed;
     }
 
-    const root_window: x11.Window = x11.DefaultRootWindow(display.?);
+    const root_window: x11.Window = x11.DefaultRootWindow(display);
     defer {
-        _ = x11.XDestroyWindow(display.?, root_window);
+        _ = x11.XDestroyWindow(display, root_window);
     }
 
     selectEvents(display, root_window);
 
-    const xim = x11.XOpenIM(display.?, null, null, null);
-    if (xim == null) {
-        std.debug.print("Cannot initialize input method\n", .{});
-        return error.X11InitializationFailed;
-    }
-    defer _ = x11.XCloseIM(xim);
-
-    const xic = x11.XCreateIC(
-        xim,
-        x11.XNInputStyle,
-        x11.XIMPreeditNothing | x11.XIMStatusNothing,
-        x11.XNClientWindow,
-        app_window,
-        x11.XNFocusWindow,
-        app_window,
-    );
-    if (xic == null) {
-        std.debug.print("Cannot initialize input context\n", .{});
-        return error.X11InitializationFailed;
-    }
-    defer x11.XDestroyIC(xic);
+    var input_ctx = try X11InputContext.init(display, app_window);
+    defer input_ctx.deinit();
 
     while (true) {
         // x11 wait for event (only key press/release selected)
@@ -182,9 +239,9 @@ fn x11Listener(app_window: x11.Window) !void {
         // blocks, makes this thread impossible to exit:
         // TODO: maybe use alarms?
         // https://nrk.neocities.org/articles/x11-timeout-with-xsyncalarm
-        _ = x11.XNextEvent(display.?, &ev);
+        _ = x11.XNextEvent(display, &ev);
 
-        if (x11.XGetEventData(display.?, cookie) != 0 and
+        if (x11.XGetEventData(display, cookie) != 0 and
             cookie.type == x11.GenericEvent and
             cookie.extension == xi_opcode)
         {
@@ -193,59 +250,21 @@ fn x11Listener(app_window: x11.Window) !void {
                     const device_event: *x11.XIDeviceEvent = @alignCast(@ptrCast(cookie.data));
                     const keycode: usize = @intCast(device_event.detail);
 
+                    if (event_file) |file| {
+                        const device_event_data: [*]u8 = @ptrCast(device_event);
+                        _ = try file.writeAll(device_event_data[0..@sizeOf(x11.XIDeviceEvent)]);
+                    }
+
                     const lookup: i32 = keycode_keyboard_lookup[keycode];
                     if (lookup >= 0) {
                         const index: usize = @intCast(lookup);
                         key_states[index].pressed = cookie.evtype == x11.XI_KeyPress;
                     }
 
-                    if (event_file) |file| {
-                        const device_event_data: [*]u8 = @ptrCast(device_event);
-                        _ = try file.writeAll(device_event_data[0..@sizeOf(x11.XIDeviceEvent)]);
-                    }
-
                     if (cookie.evtype == x11.XI_KeyPress) {
-                        var e: x11.XKeyPressedEvent = .{
-                            .type = x11.XI_KeyPress,
-                            .display = display,
-                            .window = root_window,
-                            .root = root_window,
-                            .subwindow = 0,
-                            .time = device_event.time,
-                            .x = 0,
-                            .y = 0,
-                            .x_root = 0,
-                            .y_root = 0,
-                            .state = @intCast(device_event.mods.effective),
-                            .keycode = @intCast(keycode),
-                            .same_screen = 1,
-                        };
-                        var char_buffer = [_]u8{0} ** 32;
-                        var keysym: x11.KeySym = undefined;
-                        var status: x11.Status = undefined;
-                        const len = x11.Xutf8LookupString(xic, &e, &char_buffer, 32, &keysym, &status);
                         last_char_timestamp = std.time.timestamp();
-                        std.debug.print(
-                            "time: {} status: {any} keycode: {d} keysym: 0x{X} buffer({}): '{s}'\n",
-                            .{
-                                last_char_timestamp,
-                                status,
-                                keycode,
-                                keysym,
-                                len,
-                                std.fmt.fmtSliceHexLower(&char_buffer),
-                            },
-                        );
-
-                        const key: KeyData = .{
-                            .pressed = true,
-                            .repeated = false,
-                            .keycode = @intCast(keycode),
-                            .keysym = keysym,
-                            .status = status,
-                            .symbol = x11.XKeysymToString(keysym),
-                            .string = char_buffer,
-                        };
+                        var key: KeyData = std.mem.zeroInit(KeyData, .{});
+                        _ = input_ctx.lookupString(device_event, &key);
 
                         while (!keys.push(key)) : ({
                             // this is unlikely scenario - normal typing would not be fast enough
@@ -259,11 +278,11 @@ fn x11Listener(app_window: x11.Window) !void {
             }
         }
 
-        x11.XFreeEventData(display.?, cookie);
+        x11.XFreeEventData(display, cookie);
     }
 
-    _ = x11.XSync(display.?, 0);
-    _ = x11.XCloseDisplay(display.?);
+    _ = x11.XSync(display, 0);
+    _ = x11.XCloseDisplay(display);
 }
 
 // uses events stored in file to reproduce them
@@ -273,34 +292,15 @@ fn x11Producer(app_window: x11.Window) !void {
         std.debug.print("defer x11Producer\n", .{});
     }
 
-    var display: ?*x11.Display = null;
-    display = x11.XOpenDisplay(null);
-    if (display == null) {
+    const display: *x11.Display = x11.XOpenDisplay(null) orelse {
         std.debug.print("Unable to connect to X server\n", .{});
         return error.X11InitializationFailed;
-    }
+    };
 
-    const xim = x11.XOpenIM(display.?, null, null, null);
-    if (xim == null) {
-        std.debug.print("Cannot initialize input method\n", .{});
-        return error.X11InitializationFailed;
-    }
-    defer _ = x11.XCloseIM(xim);
+    var input_ctx = try X11InputContext.init(display, app_window);
+    defer input_ctx.deinit();
 
-    const xic = x11.XCreateIC(
-        xim,
-        x11.XNInputStyle,
-        x11.XIMPreeditNothing | x11.XIMStatusNothing,
-        x11.XNClientWindow,
-        app_window,
-        x11.XNFocusWindow,
-        app_window,
-    );
-    if (xic == null) {
-        std.debug.print("Cannot initialize input context\n", .{});
-        return error.X11InitializationFailed;
-    }
-    defer x11.XDestroyIC(xic);
+    std.debug.print("Replay events from file\n", .{});
 
     const file = try std.fs.cwd().openFile("events.txt", .{});
     defer file.close();
@@ -312,75 +312,34 @@ fn x11Producer(app_window: x11.Window) !void {
     var events_count: usize = 0;
     var timestamp: x11.Time = 0; // timestamp in x11 events is in milliseconds
     var previous_timestamp: x11.Time = 0;
-    while (reader.readStruct(x11.XIDeviceEvent)) |event| {
-        timestamp = event.time;
+    while (reader.readStruct(x11.XIDeviceEvent)) |device_event| {
+        timestamp = device_event.time;
         const time_to_wait = timestamp - previous_timestamp;
-        std.debug.print("Should wait {} ms before emitting fake event\n", .{time_to_wait});
         // first would be large because it is in reference to x11 server start,
         // delay only on 1..n event
         if (events_count != 0 and time_to_wait != 0) {
             std.time.sleep(time_to_wait * std.time.ns_per_ms);
         }
-        std.debug.print("{}: event: '{any}'\n", .{timestamp, event});
 
         // do stuff with event-from-file
-        // TODO: duplicate code, refactor, probably should move
-        // x11 stuff to another file already
-        const keycode: usize = @intCast(event.detail);
+        const keycode: usize = @intCast(device_event.detail);
         const lookup: i32 = keycode_keyboard_lookup[keycode];
         if (lookup >= 0) {
             const index: usize = @intCast(lookup);
-            key_states[index].pressed = event.evtype == x11.XI_KeyPress;
+            key_states[index].pressed = device_event.evtype == x11.XI_KeyPress;
         }
-        if (event.evtype == x11.XI_KeyPress) {
-            var e: x11.XKeyPressedEvent = .{
-                .type = x11.XI_KeyPress,
-                .display = display,
-                .window = app_window,
-                .root = app_window,
-                .subwindow = 0,
-                .time = event.time,
-                .x = 0,
-                .y = 0,
-                .x_root = 0,
-                .y_root = 0,
-                .state = @intCast(event.mods.effective),
-                .keycode = @intCast(keycode),
-                .same_screen = 1,
-            };
-            var char_buffer = [_]u8{0} ** 32;
-            var keysym: x11.KeySym = undefined;
-            var status: x11.Status = undefined;
-            const len = x11.Xutf8LookupString(xic, &e, &char_buffer, 32, &keysym, &status);
-            last_char_timestamp = std.time.timestamp();
-            std.debug.print(
-                "time: {} status: {any} keycode: {d} keysym: 0x{X} buffer({}): '{s}'\n",
-                .{
-                    last_char_timestamp,
-                    status,
-                    keycode,
-                    keysym,
-                    len,
-                    std.fmt.fmtSliceHexLower(&char_buffer),
-                },
-            );
 
-            const key: KeyData = .{
-                .pressed = true,
-                .repeated = false,
-                .keycode = @intCast(keycode),
-                .keysym = keysym,
-                .status = status,
-                .symbol = x11.XKeysymToString(keysym),
-                .string = char_buffer,
-            };
+        if (device_event.evtype == x11.XI_KeyPress) {
+            last_char_timestamp = std.time.timestamp();
+            var key: KeyData = std.mem.zeroInit(KeyData, .{});
+            _ = input_ctx.lookupString(&device_event, &key);
 
             while (!keys.push(key)) : ({
                 // this is unlikely scenario - normal typing would not be fast enough
                 std.debug.print("Consumer outpaced, try again\n", .{});
                 std.time.sleep(10 * std.time.ns_per_ms);
             }) {}
-            std.debug.print("Produced: '{any}'\n", .{key});
+            std.debug.print("Produced (fake): '{any}'\n", .{key});
         }
 
         // continue with next events
@@ -392,7 +351,6 @@ fn x11Producer(app_window: x11.Window) !void {
         },
         else => return err,
     }
-
 }
 
 pub fn main() !void {
